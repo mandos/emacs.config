7360ea4cbdf8e8c1ba6d0c5243002cb7a28daf3e
Marek Maksimczyk <marek.maksimczyk@mandos.net.pl>
2026-02-11 16:02:47 +0900

    module(org-roam): Add module to manage org-roam and friends

---
+++ modules/user/org-roam/config.el
@@ -0,0 +1,63 @@
+;;; user/org-roam/config.el -*- lexical-binding: t; -*-
+
+(after! org-roam
+  (require 'org-roam-protocol)
+  (add-to-list 'magit-section-initial-visibility-alist (cons 'org-roam-node-section 'hide))
+  (setq org-roam-mode-section
+        (list #'org-roam-backlinks-section :unique t
+              #'org-roam-reflinks-section
+              #'org-roam-unlinked-references-section
+              ))
+  (setq org-roam-capture-templates
+        '(("d" "default" plain "%?" :target
+           (file+head "%<%Y%m%d%H%M%S>-${slug}.org" "#+title: ${title}\n")
+           :unnarrowed t)
+          ("y" "youtube" plain
+           (file "~/Org/templates/org-roam/youtube.org")
+           :target (file+head "%<%Y%m%d%H%M%S>-${slug}.org" "#+title: ${title}\n#+filetags: youtube\n")
+           :empty-lines-before 1
+           :unnarrowed t)
+          ))
+  (add-to-list 'magit-section-initial-visibility-alist (cons 'org-roam-node-section 'hide))
+
+  (setq org-roam-mode-section
+        (list #'org-roam-backlinks-section :unique t
+              #'org-roam-reflinks-section
+              #'org-roam-unlinked-references-section
+              ))
+  (setq org-roam-capture-templates
+        '(("d" "default" plain "%?" :target
+           (file+head "%<%Y%m%d%H%M%S>-${slug}.org" "#+title: ${title}\n")
+           :unnarrowed t)
+          ("y" "youtube" plain
+           (file "~/Org/templates/org-roam/youtube.org")
+           :target (file+head "%<%Y%m%d%H%M%S>-${slug}.org" "#+title: ${title}\n#+filetags: youtube\n")
+           :empty-lines-before 1
+           :unnarrowed t)
+          )))
+
+(use-package! websocket
+  :after org-roam)
+
+(use-package! org-roam-ui
+  :after org-roam ;; or :after org
+  :init
+
+  (map! :leader
+        :desc "Org Roam UI" "n r u" #'org-roam-ui-open)
+  (map! :map org-mode-map
+        :localleader
+        :prefix ("m")
+        (:prefix ("u". "org-roam-ui")
+         :desc "Org Roam UI" "u" #'org-roam-ui-open
+         :desc "Toggle folow nodes" "f" #'org-roam-ui-follow-mode
+         :desc "Local graph" "l" #'org-roam-ui-node-local
+         :desc "Add to local graph" "a" #'org-roam-ui-add-to-local-graph
+         :desc "Remove from local graph" "r" #'org-roam-ui-remove-from-local-graph))
+
+  :config
+  (setq org-roam-ui-sync-theme t
+        org-roam-ui-follow t
+        org-roam-ui-update-on-save t
+        org-roam-ui-open-on-start t
+        org-fold-core-style 'overlays))
---
+++ modules/user/org-roam/packages.el
@@ -0,0 +1,7 @@
+;; -*- no-byte-compile: t; -*-
+;;; priv/org-roam/packages.el
+
+(package! org-roam :pin "046822b512ffecdee7d110f73dd3a511802ca590")
+(package! org-roam-ui :pin "5ac74960231db0bf7783c2ba7a19a60f582e91ab")
+;; (package! org-roam-timestamps :pin "c4ff1e2f5b0905b5caa917249aab56ddc1de1ab3")
+(package! websocket :pin "40c208eaab99999d7c1e4bea883648da24c03be3")
