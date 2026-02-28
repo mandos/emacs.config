e4bf1f59e3f4b730c1d167660641a62a48925de7
Marek Maksimczyk <marek.maksimczyk@mandos.net.pl>
2026-02-28 11:25:27 +0900

    Add key bind to open term with tmux in proj dir

--- config.el
+++ config.el
@@ -44,6 +44,8 @@
 (map! :leader :desc "Vertical window" :n "w \\" #'+evil/window-vsplit-and-follow)
 (map! :leader :desc "Horizontal window" :n "w -" #'+evil/window-split-and-follow)
 (map! :leader :desc "Dirvish" :n "o m" #'dirvish)
+(map! :leader :desc "Terminal with Tmux" :n "o o"
+      (cmd! (start-process "alacritty" nil "alacritty" "-e" "bash" "-ic" (concat "t " (shell-quote-argument default-directory)))))
 (map! :leader :desc "Open Bibliographic note" :n "n B" #'citar-open)
 
 (map! :n "[ e" #'flymake-goto-prev-error)
